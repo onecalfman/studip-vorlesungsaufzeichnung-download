3c32a51c3e1d1636aa4f4835ce89fd4f2773c08f
Jonas Walkling <jonas@artograph.info>
2021-02-05 16:45:39 +0100

    coursware download button

--- script.js
+++ script.js
@@ -1,41 +1,9 @@
 const studip_url = window.location.origin
 const opencast_url = 'https://opencast-present.rz.tu-bs.de'
 
-function studipDownloadCreateButton(series, name, url, i) {
-	url = url.replace(studip_url, opencast_url);
 
-	button = document.createElement('button');
-	button.setAttribute('onclick', 'location.href=' + url);
-	button.setAttribute('type', 'button');
-	button.innerText = ("Download: " + name);
-
-	document.getElementsByClassName("oce_list list")[0].children[i].appendChild(button);
-	button.onclick = function(){
-		window.open(url)
-		//var downloading = browser.downloads.download({
-  		//	url : url,
-  		//	filename : name,
-  		//	conflictAction : 'uniquify'
-		//});
-	};
-}
-
-function studipDownloadParseData(res,i) {
-
-		var videoNum = 0;
-
-		if ( res["search-results"]["result"]["mediapackage"]["media"]["track"][0]["video"]["bitrate"] <= res["search-results"]["result"]["mediapackage"]["media"]["track"][1]["video"]["bitrate"]) {
-			videoNum = 1;
-		} else {
-			videoNum = 0;
-		}
-
-		series = res["search-results"]["result"]["mediapackage"]["seriestitle"]
-		name = res["search-results"]["result"]["mediapackage"]["title"]
-		url = res["search-results"]["result"]["mediapackage"]["media"]["track"][videoNum]["url"]
-
-		studipDownloadCreateButton(series, name, url, i);
-}
+if(document.URL.includes("courseware")) { coursewareDownloaderGetData() }
+else { studipDownloaderGetData(); }
 
 async function studipDownloaderGetData() {
 	var episodes = document.getElementsByClassName("oce_list list")[0].children;
@@ -48,4 +16,54 @@ async function studipDownloaderGetData() {
 	}
 }
 
-studipDownloaderGetData();
+async function coursewareDownloaderGetData() {
+	let i=0
+	id=document.getElementsByTagName("iframe")[0].src.match(/id=(.*)&/)[1]
+	if(! id.includes('-')) {
+		setTimeout(coursewareDownloaderGetData, 500);
+		return;
+	}
+	url = studip_url + '/search/episode.json?id=' + id;
+	fetch(url)
+  	.then(response => response.json())
+  	.then(data => studipDownloadParseData(data,i));
+}
+
+function studipDownloadParseData(res,i) {
+	var videoNum = 0;
+
+	if ( res["search-results"]["result"]["mediapackage"]["media"]["track"][0]["video"]["bitrate"] <= res["search-results"]["result"]["mediapackage"]["media"]["track"][1]["video"]["bitrate"]) {
+		videoNum = 1;
+	} else {
+		videoNum = 0;
+	}
+
+	series = res["search-results"]["result"]["mediapackage"]["seriestitle"]
+	name = res["search-results"]["result"]["mediapackage"]["title"]
+	url = res["search-results"]["result"]["mediapackage"]["media"]["track"][videoNum]["url"]
+	studipDownloadCreateButton(series, name, url, i);
+}
+
+function studipDownloadCreateButton(series, name, url, i) {
+	url = url.replace(studip_url, opencast_url);
+
+	button = document.createElement('button');
+	button.setAttribute('onclick', 'location.href=' + url);
+	button.setAttribute('type', 'button');
+	button.innerText = ("Download: " + name);
+
+	if(document.URL.includes("courseware")) {
+		document.getElementsByClassName('block-content')[0].appendChild(button);
+	}
+	else {
+		document.getElementsByClassName("oce_list list")[0].children[i].appendChild(button);
+	}
+	button.onclick = function(){
+		window.open(url)
+		//var downloading = browser.downloads.download({
+  		//	url : url,
+  		//	filename : name,
+  		//	conflictAction : 'uniquify'
+		//});
+	};
+}
